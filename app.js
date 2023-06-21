5861902855d4ebf0fe03a285b667005f495abad2
Venkata Sai Reddy <avulurivenkatasaireddy@gmail.com>
2023-06-21 20:27:21 +0530

    Add. BodyParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const router = express.Router();
 const cors = require("cors");
-const router = express().router();
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
@@ -8,9 +9,15 @@ const PORT = 3000;
 // https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express
 app.use(cors({ origin: "*", methods: ["POST", "GET"] }));
 
+app.use(router);
+
+// https://stackoverflow.com/a/39872729/11652193
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.status(200);
-  res.send("Welcome to root URL of Server");
+  res.send("Welcome to the Task Manager API");
 });
 
 app.listen(PORT, (error) => {
