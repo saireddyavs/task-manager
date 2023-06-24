248dd1ee8d2d869bcd54b06cd8fd1f1afebc2d99
Venkata Sai Reddy <avulurivenkatasaireddy@gmail.com>
2023-06-24 17:40:29 +0530

    Add. Morgan for request logginh

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const express = require("express");
+const express = require('express');
+
 const routes = express.Router();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const taskManager = require("./src/routes/taskManager");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const taskManager = require('./src/routes/taskManager');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3000;
 
 // https://www.section.io/engineering-education/how-to-use-cors-in-nodejs-with-express
-app.use(cors({ origin: "*", methods: ["POST", "GET", "PUT", "DELETE"] }));
-
-app.use(routes);
+app.use(cors({ origin: '*', methods: ['POST', 'GET', 'PUT', 'DELETE'] }));
 
 // https://stackoverflow.com/a/39872729/11652193
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-routes.use("/tasks", taskManager);
+// https://www.npmjs.com/package/morgan
+app.use(morgan('tiny'));
 
-app.get("/", (req, res) => {
+app.use(routes);
+
+routes.use('/tasks', taskManager);
+
+app.get('/', (req, res) => {
   res.status(200);
-  res.send("Welcome to the Task Manager API");
+  res.send('Welcome to the Task Manager API');
 });
 
 app.listen(PORT, (error) => {
   if (!error)
     console.log(
-      "Server is Successfully Running, and App is listening on port " + PORT
+      'Server is Successfully Running, and App is listening on port ' + PORT
     );
   else console.log("Error occurred, server can't start", error);
 });
